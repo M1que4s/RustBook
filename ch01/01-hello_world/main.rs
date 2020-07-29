e8d0a8ae3b3f383ba1c514bcae2a66923db7c0d6
Miqueas Martinez <miqueas2020@yahoo.com>
2020-07-29 00:51:57 -0500

    Inicializado repositorio

---
+++ ch01/01-hello_world/main.rs
@@ -0,0 +1,8 @@
+// Esto es un programa "Hello, world!" en Rust. Las funciones se crean con el keyword 'fn'
+// y 'main' es lo primero en ejecutarse en cualquier programa escrito en Rust.
+fn main() {
+  // 'println!' es una "macro", si fuera una función, se llamaría sin el ! (signo
+  // de excalamación). De esto se hablará más adelante.
+  println!("Hello, World!");
+  // Rust usa el ; (punto y coma) para finalizar las líneas de intrucciones (en su mayoría)
+}
---
+++ ch01/02-hello_cargo/src/main.rs
@@ -0,0 +1,7 @@
+// Cargo es el gestor de paquetes de y a su vez el sistema de construcción de Rust.
+// La mayoría de los proyectos en Rust usan Cargo porque facilita muchas tareas. En este caso
+// se recreará el código del capítulo 1.1 usando Cargo (cargo run/build/check son explicados
+// en este capítulo y el flag --release para compilaciones finales).
+fn main() {
+  println!("Hello, Cargo!");
+}
