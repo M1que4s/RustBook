3d862502dcecc74b46e6848be3272a47cd5b81c5
Miqueas Martinez <miqueas2020@yahoo.com>
2020-08-06 13:15:29 -0500

    Capítulo 3: creando variables mutables.

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -1,12 +1,7 @@
-// Capitulo 3: Variables y mutabilidad.
-// Por defecto, las variables son inmutables en Rust, una vez que hay un valor asignado a
-// un nombre, éste no podrá cambiarse. Rust trabaja de esta forma para darnos un "empujón"
-// a escribir mejor código. Sin embargo, las variables pueden volverse mutables si así lo
-// queremos. Se presenta este primer código en el capítulo, el cual muestra como el
-// compilador de Rust nos retorna un error debido a que no podemos cambiar el valor de "x"
+// Al igual que en el capítulo 2, podemos cambiar este comportamiento usando el keyword mut
 
 fn main() {
-  let x = 5;
+  let mut x = 5;
   println!("Value = {}", x);
   x = 6;
   println!("Value = {}", x);
