e1b671685ed1f40204edfb455fa189aea73cda58
Miqueas Martinez <miqueas2020@yahoo.com>
2020-08-06 13:27:09 -0500

    Capítulo 3: constantes.

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -1,8 +1,10 @@
-// Al igual que en el capítulo 2, podemos cambiar este comportamiento usando el keyword mut
+// En Rust también disponemos de constantes, las cuales son muy diferentes de las variables:
+//    Las constantes no se pueden hacer mutables con el keyword mut como se hace con las
+//    variables. Se debe indicar siempre el tipo de dato. Y su valor debe ser constante,
+//    es decir, no puede ser algo que se calcúle en tiempo de ejecución o llamadas a una
+//    función, etc. Adicionalmente, se recomienda usar nombres en mayúsculas para éstas.
 
 fn main() {
-  let mut x = 5;
-  println!("Value = {}", x);
-  x = 6;
-  println!("Value = {}", x);
+  const MAX_POINTS: u32 = 100_000;
+  // Rust permite usar "underscores" en valores literales numéricos para una mejor legibilidad
 }
